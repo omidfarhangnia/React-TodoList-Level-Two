fc26c17ef57ab2ff2d2c37633001ebb9ae2e1b1c
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-03-25 16:41:56 +0330

    starting the project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import FormTodoAdd from "./FormTodoAdd";
+import ShowTodoWorks from "./ShowTodoWorks";
+
+export const style = {
+  bg: ``,
+  // bg: `bg-black`,
+  // bg: `bg-black`,
+  // bg: `bg-black`,
+}
+
+export default function App() {
+  return (
+    <div className="todoContainer p-5 d-flex flex-column">
+        
+    </div>
+  );
+}
