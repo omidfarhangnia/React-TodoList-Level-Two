1a332c82df9d3840820f02c9038f6f9d86473372
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-03-27 15:26:08 +0330

    adding thunders icons

--- src/App.js
+++ src/App.js
@@ -2,21 +2,7 @@ import { useState } from "react";
 import "./App.scss";
 import BackLogs from "./BackLogs";
 import CompletedTasks from "./CompletedTasks";
-
-export const style = {
-  container: "d-flex flex-column justify-content-between",
-  // header style
-  header: "d-flex flex-column align-items-center font-roboto",
-  header_heading: "text-center text-mainBlue mb-3",
-  // date container styled
-  dateContainer: {
-    SelfStyle: "d-flex justify-content-around align-items-center",
-    heading:
-      "text-uppercase font-roboto text-mainBlack bg-mainBlue rounded-pill m-0",
-    date: "text-mainBlue font-roboto",
-  },
-  addBtn: "border-0 colorFul__btn",
-};
+import TaskAdder from "./TaskAdder";
 
 let currentDate;
 if (typeof window !== "undefined") {
@@ -60,26 +46,22 @@ export default function App() {
     energyCosts: [false, false, false]
   });
 
-  console.log(newValues.energyCosts)
+  console.log(newValues.en)
 
-
-  function handleNewValue(target) {
-    if (target.type === "checkbox") {
-      let newCheckBoxValue = [];
+  function handleNewValue(target, id) {
+    if (target.getAttribute("isThunder") === "true") {
+      let newEnergyValue = [];
       for(var i = 1; i <= 3; i++){
-        if(i <= Number(target.id)){
-          newCheckBoxValue.push(true)
+        if(i <= Number(id)){
+          newEnergyValue.push(true)
         }else{
-          newCheckBoxValue.push(false)
+          newEnergyValue.push(false)
         }
       }
 
-      console.log(target.id)
-      console.log(newCheckBoxValue)
-
       setNewValues({
         ...newValues,
-        [target.name]: newCheckBoxValue
+        energyCosts: newEnergyValue
       });
     } else {
       setNewValues({
@@ -90,7 +72,7 @@ export default function App() {
   }
 
   return (
-    <div className={`todoContainer ${style.container}`}>
+    <div className={`todoContainer d-flex flex-column justify-content-between`}>
       <HeaderSection />
       {/* it will give a button which show current task */}
       {/* <CurrentTask /> */}
@@ -110,15 +92,15 @@ export default function App() {
 
 function HeaderSection() {
   return (
-    <header className="header">
-      <h1 className={style.header_heading}>Note</h1>
-      <div className={`date__container ${style.dateContainer.SelfStyle}`}>
+    <header className="d-flex flex-column align-items-center font-roboto">
+      <h1 className="text-center text-mainBlue mb-3 text-capitalize">Note</h1>
+      <div className="date__container d-flex justify-content-around align-items-center">
         <h4
-          className={`date__container--heading ${style.dateContainer.heading}`}
+          className="date__container--heading text-uppercase font-roboto text-mainBlack bg-mainBlue rounded-pill m-0 date__header"
         >
           today
         </h4>
-        <span className={`date__container--date ${style.dateContainer.date}`}>
+        <span className="date__container--date text-mainBlue font-roboto date__header">
           {currentDate}
         </span>
       </div>
@@ -137,68 +119,10 @@ function CleanUpTasks() {
 function AddNewTask() {
   return (
     <div className="add__button__container">
-      <button className={`add__button ${style.addBtn}`}>New</button>
+      <button className={`add__button border-0 colorFul__btn`}>New</button>
     </div>
   );
 }
 
-function TaskAdder({ newValue, onChangeValue }) {
-  return (
-    <div className="taskAdderContainer d-flex flex-column justify-content-around align-items-center">
-      <form onSubmit={(e) => e.target.value} className={"d-flex flex-column"}>
-        <label>
-          name
-          <input
-            type={"text"}
-            value={newValue.name}
-            name="name"
-            onChange={(e) => onChangeValue(e.target)}
-          />
-        </label>
-        <label>
-          Description
-          <br/>
-          <textarea
-            value={newValue.description}
-            name="description"
-            onChange={(e) => onChangeValue(e.target)}
-          />
-        </label>
-        <label>
-          Priority
-          <input
-            value={newValue.priority}
-            name="priority"
-            type={"color"}
-            onChange={(e) => onChangeValue(e.target)}
-          />
-        </label>
-        <label>
-          energy costs.
-          <input
-            type={"checkbox"}
-            name="energyCosts"
-            id="1"
-            value={newValue.energyCosts[0]}
-            onChange={(e) => onChangeValue(e.target)}
-          />
-          <input
-            type={"checkbox"}
-            name="energyCosts"
-            id="2"
-            value={newValue.energyCosts[1]}
-            onChange={(e) => onChangeValue(e.target)}
-          />
-          <input
-            type={"checkbox"}
-            name="energyCosts"
-            id="3"
-            value={newValue.energyCosts[2]}
-            onChange={(e) => onChangeValue(e.target)}
-          />
-        </label>
-      </form>
-      <button className={style.addBtn}>add</button>
-    </div>
-  );
-}
+
+
---
+++ src/TaskAdder.js
@@ -0,0 +1,126 @@
+import { useState } from "react";
+
+export default function TaskAdder({ newValue, onChangeValue }) {
+  const [activeId, setActiveId] = useState(0);
+  const [isEnergyOn, setIsEnergyOn] = useState(false);
+
+  function handleChangeActiveId(id, target) {
+    onChangeValue(target, id)
+    if (id === 1) {
+      setIsEnergyOn(!isEnergyOn);
+    }
+
+    if (isEnergyOn) {
+      setActiveId(0);
+    } else {
+      setActiveId(id);
+    }
+  }
+  return (
+    <div className="taskAdderContainer d-flex flex-column justify-content-around align-items-center">
+      <div>
+        <button className="back__button">
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M14.19 0H5.81C2.17 0 0 2.17 0 5.81V14.18C0 17.83 2.17 20 5.81 20H14.18C17.82 20 19.99 17.83 19.99 14.19V5.81C20 2.17 17.83 0 14.19 0ZM11.79 13C12.08 13.29 12.08 13.77 11.79 14.06C11.64 14.21 11.45 14.28 11.26 14.28C11.07 14.28 10.88 14.21 10.73 14.06L7.2 10.53C6.91 10.24 6.91 9.76 7.2 9.47L10.73 5.94C11.02 5.65 11.5 5.65 11.79 5.94C12.08 6.23 12.08 6.71 11.79 7L8.79 10L11.79 13Z"
+              fill="#78ACD5"
+            />
+          </svg>
+        </button>
+        <h4 className="text-center text-mainBlue text-capitalize m-0">new note</h4>
+      </div>
+      <form onSubmit={(e) => e.target.value} className={"d-flex flex-column"}>
+        <label>
+          name
+          <input
+            type={"text"}
+            value={newValue.name}
+            name="name"
+            onChange={(e) => onChangeValue(e.target)}
+          />
+        </label>
+        <label>
+          Description
+          <br />
+          <textarea
+            value={newValue.description}
+            name="description"
+            onChange={(e) => onChangeValue(e.target)}
+          />
+        </label>
+        <label>
+          Priority
+          <input
+            value={newValue.priority}
+            name="priority"
+            type={"color"}
+            onChange={(e) => onChangeValue(e.target)}
+          />
+        </label>
+        <label>
+          energy costs.
+          <ThunderIcon
+            id={1}
+            activeId={activeId}
+            onChangeActiveId={handleChangeActiveId}
+          />
+          <ThunderIcon
+            id={2}
+            activeId={activeId}
+            onChangeActiveId={handleChangeActiveId}
+          />
+          <ThunderIcon
+            id={3}
+            activeId={activeId}
+            onChangeActiveId={handleChangeActiveId}
+          />
+        </label>
+      </form>
+      <button className="border-0 colorFul__btn">add</button>
+    </div>
+  );
+}
+
+function ThunderIcon({ activeId, id, onChangeActiveId }) {
+  if (id <= activeId) {
+    return (
+      <svg
+        width="20"
+        height="15"
+        isThunder="true"
+        viewBox="0 0 20 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        onClick={(e) => onChangeActiveId(id, e.target)}
+      >
+        <path
+          d="M10.1862 6.5933H7.99745V1.4933C7.99745 0.303301 7.35286 0.0624681 6.56661 0.954968L5.99995 1.59955L1.20453 7.05372C0.54578 7.79747 0.82203 8.40663 1.8137 8.40663H4.00245V13.5066C4.00245 14.6966 4.64703 14.9375 5.43328 14.045L5.99995 13.4004L10.7954 7.94622C11.4541 7.20247 11.1779 6.5933 10.1862 6.5933Z"
+          fill="#78ACD5"
+        />
+      </svg>
+    );
+  } else {
+    return (
+      <svg
+        width="20"
+        height="15"
+        isThunder="true"
+        viewBox="0 0 20 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        onClick={(e) => onChangeActiveId(id, e.target)}
+      >
+        <path
+          d="M10.1862 6.5933H7.99745V1.4933C7.99745 0.303301 7.35286 0.0624681 6.56661 0.954968L5.99995 1.59955L1.20453 7.05372C0.54578 7.79747 0.82203 8.40663 1.8137 8.40663H4.00245V13.5066C4.00245 14.6966 4.64703 14.9375 5.43328 14.045L5.99995 13.4004L10.7954 7.94622C11.4541 7.20247 11.1779 6.5933 10.1862 6.5933Z"
+          fill="white"
+        />
+      </svg>
+    );
+  }
+}
